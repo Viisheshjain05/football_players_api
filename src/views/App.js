b95b17eeb1bd4c9de0b982778c028721bd0280f5
viisheshjain05 <viisheshjain05@gmail.com>
2021-06-25 01:27:27 +0530

    init js Machine test project

--- src/views/App.js
+++ src/views/App.js
@@ -1,75 +1,64 @@
-import React, { useState } from "react";
-import FinancialCard from "../components/FinancialCard.jsx";
+import React, { useEffect, useState } from "react";
+import PlayerCard from "../components/PlayerCard.jsx";
 import axios from "axios";
 import "./App.scss";
 
 const App = () => {
-  const [CardData, setCardData] = useState([]);
+  const [PlayerData, setPlayerData] = useState([]);
 
-  const fetchData = (el) => {
+  useEffect(() => {
     axios({
       method: "GET",
-      url: `https://sandbox.iexapis.com/stable/stock/${
-        el ? el : "first"
-      }/quote?token=Tsk_678b4f8a0c3b4032b11c7568fb24dc17`,
+      url: "https://api.npoint.io/20c1afef1661881ddc9c",
     })
       .then((res) => {
-        const List = res.data;
-        // console.log(".then ~ List", List);
-        // const playerUpdatedList = [];
+        const List = res.data.playerList;
+        const playerUpdatedList = [];
 
-        setCardData({
-          CName: List.companyName,
-          Symbol: List.symbol,
-          RTPrice: List.iexRealtimePrice,
-          ChangeValue: List.change,
-          ChangePercentage: List.changePercent,
-          MCap: List.marketCap,
-          PERatio: List.peRatio,
-          ATVolume: List.avgTotalVolume,
-          PClose: List.previousClose,
-          W52High: List.week52High,
-          W52Low: List.week52Low,
+        List.forEach((el) => {
+          playerUpdatedList.push({
+            id: el.Id,
+            PFName: el.PFName,
+            TName: el.TName,
+            SkillDesc: el.SkillDesc,
+            Value: el.Value,
+            UpComingMatches: {
+              CCode: el.UpComingMatchesList[0].CCode,
+              VsCCode: el.UpComingMatchesList[0].VsCCode,
+              MDate: el.UpComingMatchesList[0].MDate,
+            },
+          });
         });
-      })
-      .catch((err) => setCardData("error"));
-  };
 
-  console.log("CardData", CardData);
+        setPlayerData(playerUpdatedList);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // console.log(PlayerData);
 
   return (
     <>
-      <div className="search-bar">
-        <input
-          type="text"
-          className="search-bar__input"
-          placeholder="Stock Ticker"
-          onChange={(e) => fetchData(e.target.value)}
-        />
-        {/* <button className="search-bar__button">search</button> */}
+      <div className="player__search-bar--head">
+        <input type="text" name="" id="" placeholder="Search Players" />
       </div>
-      {CardData.CName && (
-        <div className="card__section--head">
-          <div className="card">
-            <React.Fragment>
-              <FinancialCard data={CardData} />
+
+      <div className="player__list--head">
+        {PlayerData.map((el, ind) => {
+          return (
+            <React.Fragment key={el.id}>
+              <PlayerCard
+                player_img={el.id}
+                name={el.PFName}
+                skills={el.SkillDesc}
+                price={el.Value}
+                matches={el.UpComingMatches}
+              />
             </React.Fragment>
-          </div>
-        </div>
-      )}
-
-      {CardData.length === 0 && (
-        <>
-          <div className="error__start"> Search stock by ticker symbol.</div>
-        </>
-      )}
-      {CardData === "error" && (
-        <>
-          <div className="error__start"> Invalid ticker symbol. </div>
-        </>
-
-
-      )}
+          );
+        })}
+      </div>
+      <div className="">hii</div>
     </>
   );
 };
