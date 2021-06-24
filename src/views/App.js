98150f271e959c979c6458eae52e029dbc930df7
viisheshjain05 <viisheshjain05@gmail.com>
2021-06-24 22:15:28 +0530

    stock market card complete

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import PlayerCard from "../components/PlayerCard.jsx";
+import FinancialCard from "../components/FinancialCard.jsx";
 import axios from "axios";
 import "./App.scss";
 
 const App = () => {
-  const [PlayerData, setPlayerData] = useState([]);
+  const [CardData, setCardData] = useState([]);
 
   useEffect(() => {
     axios({
@@ -12,53 +12,44 @@ const App = () => {
       url: "https://sandbox.iexapis.com/stable/stock/aapl/quote?token=Tsk_678b4f8a0c3b4032b11c7568fb24dc17",
     })
       .then((res) => {
-        const List = res.data.playerList;
-        const playerUpdatedList = [];
+        const List = res.data;
+        // console.log(".then ~ List", List);
+        // const playerUpdatedList = [];
 
-        List.forEach((el) => {
-          playerUpdatedList.push({
-            id: el.Id,
-            PFName: el.PFName,
-            TName: el.TName,
-            SkillDesc: el.SkillDesc,
-            Value: el.Value,
-            UpComingMatches: {
-              CCode: el.UpComingMatchesList[0].CCode,
-              VsCCode: el.UpComingMatchesList[0].VsCCode,
-              MDate: el.UpComingMatchesList[0].MDate,
-            },
-          });
+        setCardData({
+          CName: List.companyName,
+          Symbol: List.symbol,
+          RTPrice: List.iexRealtimePrice,
+          ChangeValue: List.change,
+          ChangePercentage: List.changePercent,
+          MCap: List.marketCap,
+          PERatio: List.peRatio,
+          ATVolume: List.avgTotalVolume,
+          PClose: List.previousClose,
+          W52High: List.week52High,
+          W52Low: List.week52Low,
         });
-
-        setPlayerData(playerUpdatedList);
       })
       .catch((err) => console.log(err));
   }, []);
 
-  // console.log(PlayerData);
+  // console.log(CardData);
 
   return (
     <>
-      <div className="player__search-bar--head">
-        <input type="text" name="" id="" placeholder="Search Players" />
+      <div className="search-bar">
+        <input type="text" className="search-bar__input" placeholder="AAPL" />
+        <button className="search-bar__button">search</button>
       </div>
-
-      <div className="player__list--head">
-        {PlayerData.map((el, ind) => {
-          return (
-            <React.Fragment key={el.id}>
-              <PlayerCard
-                player_img={el.id}
-                name={el.PFName}
-                skills={el.SkillDesc}
-                price={el.Value}
-                matches={el.UpComingMatches}
-              />
+      {CardData.CName &&  (
+        <div className="card__section--head">
+          <div className="card">
+            <React.Fragment>
+              <FinancialCard data={CardData} />
             </React.Fragment>
-          );
-        })}
-      </div>
-      <div className="">hii</div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
