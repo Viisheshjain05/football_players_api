4ddd112502de42f1b4a1bb9f07acae0498784879
viisheshjain05 <viisheshjain05@gmail.com>
2021-06-24 14:59:19 +0530

    added Api to fetch data

--- src/components/playerCard.jsx
+++ src/components/playerCard.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import "./PlayerCard.scss";
 
 const PlayerCard = (props) => {
-  const { player_img ,  } = props
-      return (
-    <>
-      <div  className="player-card--head" ></div>
-      <div  className="player__image" ></div>
-    </>
+  const { player_img, name, skills, price, matches } = props;
+  const { CCode, VsCCode, MDate } = matches;
+  //   console.log("PlayerCard ~ matches", matches);
+
+  return (
+    <React.Fragment key={player_img}>
+      <div className="player">
+        <div className="player__image">{player_img}</div>
+        <div className="player__name">{name}</div>
+        <div className="player__skills">{skills}</div>
+        <div className="player__price">$ {price}</div>
+        <div className="player__CCode"> {CCode}</div>
+        <div className="player__VsCCode"> {VsCCode}</div>
+        <div className="player__MDate"> {MDate}</div>
+      </div>
+    </React.Fragment>
   );
 };
 
