ba6cadd40fe460e8bc12cbf8395a927dfcd250be
viisheshjain05 <viisheshjain05@gmail.com>
2021-06-25 01:35:24 +0530

    minor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./views/App";
-import "./Reset.scss";
+import "./reset.scss";
 
 ReactDOM.render(
   <React.StrictMode>
